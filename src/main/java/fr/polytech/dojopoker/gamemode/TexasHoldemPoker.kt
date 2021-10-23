0fdf7a2dea18478f0c8bef84e2537124ca58513f
ThomasFarineau <pro.thomas06fr@gmail.com>
2021-10-23 15:55:24 +0200

    add TexasHoldemPoker.kt basics

--- src/main/java/fr/polytech/dojopoker/gamemode/TexasHoldemPoker.kt
+++ src/main/java/fr/polytech/dojopoker/gamemode/TexasHoldemPoker.kt
@@ -1,8 +1,27 @@
 package fr.polytech.dojopoker.gamemode
 
 class TexasHoldemPoker : GamePoker() {
+
+
     init {
         println("\n\n▀█▀ █▀▀ ▀▄▀ ▄▀▄ █▀▀ █▄█ █▀█ █   █▀▄ █▀▀ █▄ ▄█ █▀█ █▀█ █▄▀ █▀▀ █▀█\n█  ██▄ █ █ █▀█ ▄██ █ █ █▄█ █▄▄ █▄▀ ██▄ █ ▀ █ █▀▀ █▄█ █ █ ██▄ █▀▄\n\n")
         players = super.selectPlayers()
     }
+
+
+    fun stepFlop() {
+        //3 cartes
+        print("Flop")
+    }
+
+    fun stepTurn() {
+        //1 cartes
+        print("Turn")
+    }
+
+    fun stepRiver() {
+        //1 cartes
+        print("River")
+    }
+
 }
